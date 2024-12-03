9f743c51042249c1f68d525ce073f3b5895ea634
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-12-03 20:03:55 +0100

    update subset names

--- scripts/load_osf.py
+++ scripts/load_osf.py
@@ -1,14 +1,14 @@
 import requests
 from pathlib import Path
 
-data1 = ("data1.csv", "https://osf.io/yzqf8/download/")
-data2 = ("data2.csv", "https://osf.io/4x39f/download/")
-data3a = ("data3a.csv", "https://osf.io/r8wq3/download/")
-data3b = ("data3b.xlsx", "https://osf.io/ejg9r/download/")
-data3c = ("data3c.csv", "https://osf.io/36rhe/download/")
-data4 = ("data4.csv", "https://osf.io/7c4nf/download/")
+synergy_replication = ("synergy_replication.csv", "https://osf.io/yzqf8/download/")
+comprehensive_search = ("comprehensive_search.csv", "https://osf.io/4x39f/download/")
+inclusion_criteria_top1000 = ("inclusion_criteria_top1000.csv", "https://osf.io/r8wq3/download/")
+included_records = ("included_records.xlsx", "https://osf.io/ejg9r/download/")
+included_records_active_learning = ("included_records_active_learning.csv", "https://osf.io/36rhe/download/")
+snowballing = ("snowballing.csv", "https://osf.io/7c4nf/download/")
 
-data_subsets = [data1, data2, data3a, data3b, data3c, data4]
+data_subsets = [synergy_replication, comprehensive_search, inclusion_criteria_top1000, included_records, included_records_active_learning, snowballing]
 
 def download_data(data_subsets):
     for data_subset in data_subsets:
