c54c1c30f4c46651284a979bf59848d0509e65f1
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-12-04 17:41:40 +0100

    improve download function

--- scripts/load_osf.py
+++ scripts/load_osf.py
@@ -12,12 +12,20 @@ snowballing: tuple = ("snowballing.csv", "https://osf.io/7c4nf/download/")
 data_subsets = [synergy_replication, comprehensive_search, inclusion_criteria_top1000, included_records, 
                 included_records_active_learning, snowballing]
 
+
 def download_data(data_subsets: List[tuple]) -> None:
     for data_subset in data_subsets:
         filename, url = data_subset
-        response = requests.get(url)
-        with open(Path("data", filename), "wb") as file:
-            file.write(response.content)
+        file_path = Path("data", filename)
+        if not file_path.exists():
+            response = requests.get(url)
+            file_path.parent.mkdir(parents=True, exist_ok=True)
+            with open(Path("data", filename), "wb") as file:
+                file.write(response.content)
+            print(f"Downloaded {filename}")
+        else:
+            print(f"{filename} already exists")
+
 
 def main():
     download_data(data_subsets)
